56d30a6b672b4d85f297ab28b4b5bca7257ed1de
Jim King14 <jim@jim.jimking14>
2023-11-26 11:56:01 +0300

    I created a new routes folder and users.js file

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,7 +4,14 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
-dotenv.config();
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true})
+
+//middleware
+app.use(express.json());
+app.use(helmet())
+app.use(morgan("common"))
 
 
 app.listen(8800, ()=>{
