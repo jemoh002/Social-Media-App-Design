01c4b8d970b167523d6a6f8d376fdad9b808306d
jemoh002 <jamesnyajuaya@gmail.com>
2023-11-26 14:23:17 +0300

    Created User model and auth.js. Both files were updated, enabling full CRUD operation on a User

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true})
@@ -12,6 +14,9 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true})
 app.use(express.json());
 app.use(helmet())
 app.use(morgan("common"))
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, ()=>{
