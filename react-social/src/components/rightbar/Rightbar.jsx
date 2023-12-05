667bdfc52a0017ed072d7e14d3c5e26a703b4c78
jemoh002 <jamesnyajuaya@gmail.com>
2023-12-05 18:21:16 +0300

    Added code to the already created rightbar component. Did some css too

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,69 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText">
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+                    </span>
+                </div>
+                <img src="assets/tigerad.jpg" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li>
+
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li><li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/Rose-Muhando-I-1320x742.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Rose Muhando</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
