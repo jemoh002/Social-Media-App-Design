03f2cdffd05c7296006354bec2fc60e40f19bef5
jemoh002 <jamesnyajuaya@gmail.com>
2023-12-20 20:46:42 +0300

    Completed the profile page, rightbar aspect

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -25,10 +25,60 @@ export default function Rightbar({ profile }) {
             </>
         )
     }
+
+    const ProfileRightbar = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User Information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City: </span>
+                        <span className="rightbarInfoValue">New York</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From: </span>
+                        <span className="rightbarInfoValue">Madrid</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship: </span>
+                        <span className="rightbarInfoValue">Single</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User Friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/7.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">John Carter</span>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
         <div className="rightbar">
             <div className="rightbarWrapper">
-                
+                <ProfileRightbar/>
             </div>
         </div>
     )
