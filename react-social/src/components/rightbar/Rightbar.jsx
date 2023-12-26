23221af693e2b4180f97ec7c748914be8f81a611
jemoh002 <jamesnyajuaya@gmail.com>
2023-12-26 09:55:35 +0300

    Added a follow button in rightbar.js and styled it

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./rightbar.css"
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { Add } from "@material-ui/icons";
 
 export default function Rightbar({ user }) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER
+    const [friends, setFriends] = useState([])
+    const { user: currentUser } = useContext(AuthContext)
+    const [followed, setFollowed] = useState(false)
 
     
+    useEffect(() => {
+        const getFriends = async () => {
+            try {
+                const friendList = await axios.get("/users/friends/" + user._id);
+                setFriends(friendList.data)
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        getFriends();
+    }, [user._id])
+
+    const handleClick = async () => {
+        try {
+            
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     const HomeRightbar = () => {
         return (
             <>
@@ -31,6 +58,11 @@ export default function Rightbar({ user }) {
     const ProfileRightbar = () => {
         return (
             <>
+                {user.username !== currentUser.username && (
+                    <button className="rightbarFollowButton" onClick={handleClick}>
+                        Follow <Add/>
+                    </button>
+                )}
                 <h4 className="rightbarTitle">User Information</h4>
                 <div className="rightbarInfo">
                     <div className="rightbarInfoItem">
@@ -50,10 +82,22 @@ export default function Rightbar({ user }) {
                 </div>
                 <h4 className="rightbarTitle">User Friends</h4>
                 <div className="rightbarFollowings">
-                    <div className="rightbarFollowing">
-                        <img src={`${PF}person/1.jpeg`} alt="" className="rightbarFollowingImg" />
-                        <span className="rightbarFollowingName">John Carter</span>
-                    </div>
+                    {friends.map((friend) => (
+                        <Link to={"/profile/"+friend.username} style={{textDecoration:"none"}}>
+                            <div className="rightbarFollowing">
+                                <img src=
+                                    {
+                                        friend.profilePicture
+                                        ? PF + "person/" + friend.profilePicture
+                                        : PF + "person/noAvatar.jpg"
+                                    }
+                                    alt="" className="rightbarFollowingImg" />
+                                <span className="rightbarFollowingName">{friend.username }</span>
+                            </div>
+                        </Link>
+                        
+                    ))}
+                    
                 </div>
             </>
         )
